eac235e9d7ecf02c9bae472eeb2f6cec49c84c40
phhaus <61667103+phhaus@users.noreply.github.com>
2021-01-16 09:56:47 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='snake_rl',
+      version='0.0.1',
+      author="Philipp Hausenblas, Daniel Schmaus",
+      install_requires=['gym', 'numpy', 'matplotlib'],
+      python_requires='>=3'
+)
